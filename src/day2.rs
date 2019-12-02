e7222b55a29f35ff1288c684093cca765fdcdccb
DeltaPHC <phclem@protonmail.com>
2019-12-02 09:53:11 -0800

    Day 2, part 2, plus refactoring

--- src/day2.rs
+++ src/day2.rs
@@ -1,14 +1,16 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[aoc(day2, part1)]
-fn part1(input: &str) -> usize {
-    let mut program: Vec<usize> = input
+#[aoc_generator(day2)]
+fn day2_gen(input: &str) -> Vec<usize> {
+    input
         .split(',')
         .flat_map(|num| num.parse::<usize>().ok())
-        .collect();
+        .collect()
+}
 
-    program[1] = 12;
-    program[2] = 2;
+fn run_program(program: &mut [usize], input1: usize, input2: usize) -> usize {
+    program[1] = input1;
+    program[2] = input2;
 
     for i in (0..program.len()).step_by(4) {
         match program[i] {
@@ -30,4 +32,24 @@ fn part1(input: &str) -> usize {
     }
 
     program[0]
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &[usize]) -> usize {
+    let mut program = input.to_vec();
+    run_program(&mut program, 12, 2)
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &[usize]) -> usize {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut program = input.to_vec();
+            let num = run_program(&mut program, noun, verb);
+            if num != 19690720 { continue; }
+            return 100 * noun + verb;
+        }
+    }
+
+    panic!("No pair was found.")
 }
