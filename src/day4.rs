cf771035983667060f32df7e46d2b0b1a5e7e8b3
DeltaPHC <phclem@protonmail.com>
2019-12-03 21:37:47 -0800

    Day 4 part 1 solved. And it's terrible.

---
+++ src/day4.rs
@@ -0,0 +1,28 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::str::FromStr;
+
+#[aoc(day4, part1)]
+fn part1(input: &str) -> usize {
+    let mut split_iter = input.split('-').flat_map(|num_str| num_str.parse::<u32>());
+    let (lower, upper) = (split_iter.next().unwrap(), split_iter.next().unwrap());
+
+    (lower..=upper)
+        .filter(|&num| {
+            let num_str = num.to_string();
+            let mut prev_char = ' ';
+            let mut double_digit = false;
+            let mut never_decrease = true;
+            for i in 1..num_str.len() {
+                let ch = char::from_str(&num_str[i..=i]).unwrap();
+                let prev_ch = char::from_str(&num_str[(i - 1)..=(i - 1)]).unwrap();
+                if ch == prev_ch {
+                    double_digit = true;
+                }
+                if ch < prev_ch {
+                    never_decrease = false;
+                }
+            }
+            double_digit && never_decrease
+        })
+        .count()
+}
