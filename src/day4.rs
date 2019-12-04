4917608e4e3d5fafb57d149b2545a2078910c69d
DeltaPHC <phclem@protonmail.com>
2019-12-04 10:15:12 -0800

    Unsolved day4 part2. I feel like I'm almost there

--- src/day4.rs
+++ src/day4.rs
@@ -25,3 +25,42 @@ fn part1(input: &str) -> usize {
         })
         .count()
 }
+
+#[aoc(day4, part2)]
+fn part2(input: &str) -> usize {
+    let mut split_iter = input.split('-').flat_map(|num_str| num_str.parse::<u32>());
+    let (lower, upper) = (split_iter.next().unwrap(), split_iter.next().unwrap());
+
+    (lower..=upper)
+        .filter(|&num| {
+            let num_str = num.to_string();
+            let mut double_digit = false;
+            let mut never_decrease = true;
+            let mut num_matches = 0;
+            for i in 1..num_str.len() {
+                let ch = char::from_str(&num_str[i..=i]).unwrap();
+                let prev_ch = char::from_str(&num_str[(i - 1)..=(i - 1)]).unwrap();
+                if ch == prev_ch {
+                    num_matches += 1;
+                }
+                else {
+                    if num_matches > 0 {
+                        double_digit = ((num_matches + 1) % 2) == 0;
+                        if !double_digit { break; }
+                        num_matches = 0;
+                    }
+                }
+                if ch < prev_ch {
+                    never_decrease = false;
+                }
+            }
+
+            if num_matches > 0 {
+                double_digit = ((num_matches + 1) % 2) == 0;
+            }
+            
+            if double_digit && never_decrease { print!("{} ", num); }
+            double_digit && never_decrease
+        })
+        .count()
+}
