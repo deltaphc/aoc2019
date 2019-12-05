8c9cde1534e37a04dfa4d3c0061480f07c16f125
DeltaPHC <phclem@protonmail.com>
2019-12-05 10:07:16 -0800

    Solved day5, both parts. Takes input from keyboard

--- src/day5.rs
+++ src/day5.rs
@@ -13,6 +13,10 @@ enum Op {
     Multiply,
     Input,
     Output,
+    JumpIfTrue,
+    JumpIfFalse,
+    LessThan,
+    Equals,
     Halt,
 }
 
@@ -45,6 +49,10 @@ fn decode_instr(instr: i32) -> Instruction {
         2 => (Op::Multiply, 4),
         3 => (Op::Input, 2),
         4 => (Op::Output, 2),
+        5 => (Op::JumpIfTrue, 3),
+        6 => (Op::JumpIfFalse, 3),
+        7 => (Op::LessThan, 4),
+        8 => (Op::Equals, 4),
         99 => (Op::Halt, 1),
         _ => panic!("Illegal instruction {}", instr),
     };
@@ -70,6 +78,7 @@ fn run_intcode(prog: &mut [i32]) {
     let mut halted = false;
 
     while !halted {
+        let mut pc_increase = true;
         let instruction = decode_instr(prog[pc]);
         match instruction.opcode {
             Op::Add => {
@@ -94,7 +103,7 @@ fn run_intcode(prog: &mut [i32]) {
                     .expect("Intcode input error");
                 
                 let write_idx = prog[pc + 1] as usize;
-                prog[write_idx] = input_buf.parse::<i32>()
+                prog[write_idx] = input_buf.trim().parse::<i32>()
                     .expect("Invalid user input");
             },
             Op::Output => {
@@ -102,18 +111,69 @@ fn run_intcode(prog: &mut [i32]) {
                 let value = read_value(prog, i1, instruction.param_modes[0]);
                 println!("{}", value);
             },
+            Op::JumpIfTrue => {
+                let i1 = prog[pc + 1];
+                let i2 = prog[pc + 2];
+                let value = read_value(prog, i1, instruction.param_modes[0]);
+                let dest = read_value(prog, i2, instruction.param_modes[1]);
+                if value != 0 {
+                    pc = dest as usize;
+                    pc_increase = false;
+                }
+            },
+            Op::JumpIfFalse => {
+                let i1 = prog[pc + 1];
+                let i2 = prog[pc + 2];
+                let value = read_value(prog, i1, instruction.param_modes[0]);
+                let dest = read_value(prog, i2, instruction.param_modes[1]);
+                if value == 0 {
+                    pc = dest as usize;
+                    pc_increase = false;
+                }
+            },
+            Op::LessThan => {
+                let i1 = prog[pc + 1];
+                let i2 = prog[pc + 2];
+                let write_idx = prog[pc + 3] as usize;
+                prog[write_idx] = (
+                    read_value(prog, i1, instruction.param_modes[0]) <
+                    read_value(prog, i2, instruction.param_modes[1])) as i32;
+            },
+            Op::Equals => {
+                let i1 = prog[pc + 1];
+                let i2 = prog[pc + 2];
+                let write_idx = prog[pc + 3] as usize;
+                prog[write_idx] = (
+                    read_value(prog, i1, instruction.param_modes[0]) ==
+                    read_value(prog, i2, instruction.param_modes[1])) as i32;
+            },
             Op::Halt => halted = true,
         }
-        pc += instruction.length;
+
+        if pc_increase {
+            pc += instruction.length;
+        }
     }
 }
 
 #[aoc_generator(day5)]
-fn day5_gen(input: &str) -> i32 {
-    42
+fn day5_gen(input: &str) -> Vec<i32> {
+    input
+        .split(',')
+        .flat_map(|num_str| num_str.parse::<i32>())
+        .collect()
 }
 
 #[aoc(day5, part1)]
-fn part1(foo: &i32) -> i32 {
-    69
+fn part1(input: &[i32]) -> i32 {
+    let mut prog = input.to_vec();
+    run_intcode(&mut prog);
+    -6969 //garbage value to satisfy cargo-aoc
+}
+
+#[aoc(day5, part2)]
+fn part2(input: &[i32]) -> i32 {
+    let mut prog = input.to_vec();
+    run_intcode(&mut prog);
+    -6969 //garbage value to satisfy cargo-aoc
 }
