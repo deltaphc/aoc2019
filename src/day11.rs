f10e7074b8c5ff2bb7f7678883b9d97f15483316
DeltaPHC <phclem@protonmail.com>
2019-12-11 11:55:12 -0800

    Day11: Cleanup

--- src/day11.rs
+++ src/day11.rs
@@ -2,6 +2,13 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use crate::intcode::{Program, IOOperation, IOReturn, ExecuteAction};
 use std::collections::HashMap;
 
+mod dir {
+    pub const UP: i8 = 0;
+    pub const RIGHT: i8 = 1;
+    pub const DOWN: i8 = 2;
+    pub const LEFT: i8 = 3;
+}
+
 #[derive(Debug, Copy, Clone)]
 enum Tile {
     Black,
@@ -24,95 +31,18 @@ enum OutputMode {
     Turn,
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-#[aoc_generator(day11)]
-fn day11_gen(input: &str) -> Vec<i64> {
-    input
-        .split(',')
-        .flat_map(|num_str| num_str.trim().parse::<i64>())
-        .collect()
-}
-
-#[aoc(day11, part1)]
-fn part1(input: &[i64]) -> usize {
+/// Runs the hull painting robot and returns a tuple containing the number of tiles painted at least once, and a printable String with the output image, respectively.
+fn paint_hull(input: &[i64], starting_tile: Tile) -> (usize, String) {
     let mut prog = Program::from(input);
     let mut tiles: HashMap<(i32, i32), Tile> = HashMap::new();
-    let mut robot_x = 0;
-    let mut robot_y = 0;
-    let mut robot_dir = 0_i8; // 0 - Up, 1 - Right, 2 - Down, 3 - Left
-    let mut output_mode = OutputMode::Paint;
-    
-    while !prog.is_halted() {
-        prog.run(|io_op| {
-            match io_op {
-                IOOperation::Input => {
-                    IOReturn::Input(
-                        if tiles.contains_key(&(robot_x, robot_y)) {
-                            match tiles[&(robot_x, robot_y)] {
-                                Tile::Black => 0,
-                                Tile::White => 1,
-                            }
-                        }
-                        else {
-                            0
-                        }
-                    )
-                },
-                IOOperation::Output(value) => {
-                    match output_mode {
-                        OutputMode::Paint => {
-                            tiles.entry((robot_x, robot_y))
-                                .and_modify(|tile| *tile = Tile::from(value))
-                                .or_insert(Tile::from(value));
-                            output_mode = OutputMode::Turn;
-                        },
-                        OutputMode::Turn => {
-                            match value {
-                                // Turn left
-                                0 => robot_dir = (robot_dir - 1).rem_euclid(4),
-                                // Turn right
-                                1 => robot_dir = (robot_dir + 1).rem_euclid(4),
-                                _ => panic!("Invalid direction output {}", value),
-                            }
-
-                            match robot_dir {
-                                0 => { robot_y += 1; },
-                                1 => { robot_x += 1; },
-                                2 => { robot_y -= 1; },
-                                3 => { robot_x -= 1; },
-                                _ => panic!("Robot direction out of bounds: {}", robot_dir),
-                            }
-
-                            output_mode = OutputMode::Paint;
-                        }
-                    }
-                    IOReturn::Output(ExecuteAction::Continue)
-                },
-            }
-        });
-    }
-    tiles.len()
-}
-
-#[aoc(day11, part2)]
-fn part2(input: &[i64]) -> String {
-    let mut prog = Program::from(input);
-    let mut tiles: HashMap<(i32, i32), Tile> = HashMap::new();
-    tiles.insert((0, 0), Tile::White);
+    tiles.insert((0, 0), starting_tile);
     let mut min_width = 1;
     let mut max_width = 1;
     let mut min_height = 1;
     let mut max_height = 1;
     let mut robot_x = 0;
     let mut robot_y = 0;
-    let mut robot_dir = 0_i8; // 0 - Up, 1 - Right, 2 - Down, 3 - Left
+    let mut robot_dir = dir::UP;
     let mut output_mode = OutputMode::Paint;
     let mut output_image = String::new();
     
@@ -150,18 +80,10 @@ fn part2(input: &[i64]) -> String {
                             }
 
                             match robot_dir {
-                                0 => { // up
-                                    robot_y -= 1;
-                                },
-                                1 => { // right
-                                    robot_x += 1;
-                                },
-                                2 => { // down
-                                    robot_y += 1;
-                                },
-                                3 => { // left
-                                    robot_x -= 1;
-                                },
+                                dir::UP => { robot_y -= 1; },
+                                dir::RIGHT => { robot_x += 1; },
+                                dir::DOWN => { robot_y += 1; },
+                                dir::LEFT => { robot_x -= 1; },
                                 _ => panic!("Robot direction not within 0-3: {}", robot_dir),
                             }
 
@@ -189,12 +111,32 @@ fn part2(input: &[i64]) -> String {
             output_image.push(
                 match tiles[&(x, y)] {
                     Tile::Black => ' ',
-                    Tile::White => '*',
+                    Tile::White => '\u{2588}',
                 }
             );
         }
         output_image.push('\n');
     }
 
+    (tiles.len(), output_image)
+}
+
+#[aoc_generator(day11)]
+fn day11_gen(input: &str) -> Vec<i64> {
+    input
+        .split(',')
+        .flat_map(|num_str| num_str.trim().parse::<i64>())
+        .collect()
+}
+
+#[aoc(day11, part1)]
+fn part1(input: &[i64]) -> usize {
+    let (tiles_painted, _) = paint_hull(input, Tile::Black);
+    tiles_painted
+}
+
+#[aoc(day11, part2)]
+fn part2(input: &[i64]) -> String {
+    let (_, output_image) = paint_hull(input, Tile::White);
     output_image
 }
