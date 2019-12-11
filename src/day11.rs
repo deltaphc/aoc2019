2fd547f338dcc57564fedf2d039a91e660e813cf
DeltaPHC <phclem@protonmail.com>
2019-12-11 10:01:56 -0800

    Solved day11 part2

--- src/day11.rs
+++ src/day11.rs
@@ -89,7 +89,7 @@ fn part1(input: &[i64]) -> usize {
                                 3 => { robot_x -= 1; },
                                 _ => panic!("Robot direction out of bounds: {}", robot_dir),
                             }
-                            
+
                             output_mode = OutputMode::Paint;
                         }
                     }
@@ -99,4 +99,102 @@ fn part1(input: &[i64]) -> usize {
         });
     }
     tiles.len()
+}
+
+#[aoc(day11, part2)]
+fn part2(input: &[i64]) -> String {
+    let mut prog = Program::from(input);
+    let mut tiles: HashMap<(i32, i32), Tile> = HashMap::new();
+    tiles.insert((0, 0), Tile::White);
+    let mut min_width = 1;
+    let mut max_width = 1;
+    let mut min_height = 1;
+    let mut max_height = 1;
+    let mut robot_x = 0;
+    let mut robot_y = 0;
+    let mut robot_dir = 0_i8; // 0 - Up, 1 - Right, 2 - Down, 3 - Left
+    let mut output_mode = OutputMode::Paint;
+    let mut output_image = String::new();
+    
+    while !prog.is_halted() {
+        prog.run(|io_op| {
+            match io_op {
+                IOOperation::Input => {
+                    IOReturn::Input(
+                        if tiles.contains_key(&(robot_x, robot_y)) {
+                            match tiles[&(robot_x, robot_y)] {
+                                Tile::Black => 0,
+                                Tile::White => 1,
+                            }
+                        }
+                        else {
+                            0
+                        }
+                    )
+                },
+                IOOperation::Output(value) => {
+                    match output_mode {
+                        OutputMode::Paint => {
+                            tiles.entry((robot_x, robot_y))
+                                .and_modify(|tile| *tile = Tile::from(value))
+                                .or_insert(Tile::from(value));
+                            output_mode = OutputMode::Turn;
+                        },
+                        OutputMode::Turn => {
+                            match value {
+                                // Turn left
+                                0 => robot_dir = (robot_dir - 1).rem_euclid(4),
+                                // Turn right
+                                1 => robot_dir = (robot_dir + 1).rem_euclid(4),
+                                _ => panic!("Invalid direction output {}", value),
+                            }
+
+                            match robot_dir {
+                                0 => { // up
+                                    robot_y -= 1;
+                                },
+                                1 => { // right
+                                    robot_x += 1;
+                                },
+                                2 => { // down
+                                    robot_y += 1;
+                                },
+                                3 => { // left
+                                    robot_x -= 1;
+                                },
+                                _ => panic!("Robot direction not within 0-3: {}", robot_dir),
+                            }
+
+                            min_width = robot_x.min(min_width);
+                            max_width = robot_x.max(max_width);
+                            min_height = robot_y.min(min_height);
+                            max_height = robot_y.max(max_height);
+
+                            output_mode = OutputMode::Paint;
+                        }
+                    }
+                    IOReturn::Output(ExecuteAction::Continue)
+                },
+            }
+        });
+    }
+
+    output_image.push('\n');
+    for y in min_height..=max_height {
+        for x in min_width..=max_width {
+            if !tiles.contains_key(&(x, y)) {
+                output_image.push(' ');
+                continue;
+            }
+            output_image.push(
+                match tiles[&(x, y)] {
+                    Tile::Black => ' ',
+                    Tile::White => '*',
+                }
+            );
+        }
+        output_image.push('\n');
+    }
+
+    output_image
 }
